6d1c94673ac09e574509f8431b351176ffffbbd5
ajxlf <48630939+ajxlf@users.noreply.github.com>
2026-09-19 15:59:20 +0100

    chore: add CyberDrive/Map/NeonPalette.swift

---
+++ CyberDrive/Map/NeonPalette.swift
@@ -0,0 +1,11 @@
+import UIKit
+
+enum NeonPalette {
+    static let background = UIColor(red: 0.015, green: 0.012, blue: 0.035, alpha: 1)
+    static let panel = UIColor(red: 0.06, green: 0.04, blue: 0.10, alpha: 0.96)
+    static let cyan = UIColor(red: 0.10, green: 0.95, blue: 1.00, alpha: 1)
+    static let magenta = UIColor(red: 1.00, green: 0.13, blue: 0.78, alpha: 1)
+    static let violet = UIColor(red: 0.54, green: 0.20, blue: 1.00, alpha: 1)
+    static let text = UIColor(red: 0.88, green: 0.93, blue: 1.0, alpha: 1)
+    static let dimText = UIColor(red: 0.53, green: 0.58, blue: 0.68, alpha: 1)
+}
