0277fd0d87ec1bb8358897cc819589fde42c27cd
ajxlf <48630939+ajxlf@users.noreply.github.com>
2026-09-19 15:58:23 +0100

    chore: add CyberDrive/App/CyberDriveAppDelegate.swift

---
+++ CyberDrive/App/CyberDriveAppDelegate.swift
@@ -0,0 +1,23 @@
+import UIKit
+import AVFoundation
+
+final class CyberDriveAppDelegate: NSObject, UIApplicationDelegate {
+    func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil
+    ) -> Bool {
+        try? AVAudioSession.sharedInstance().setCategory(.playback, mode: .voicePrompt, options: [.duckOthers, .allowBluetoothA2DP])
+        return true
+    }
+
+    func application(
+        _ application: UIApplication,
+        configurationForConnecting connectingSceneSession: UISceneSession,
+        options: UIScene.ConnectionOptions
+    ) -> UISceneConfiguration {
+        if connectingSceneSession.role == .carTemplateApplication {
+            return UISceneConfiguration(name: "CarPlay Configuration", sessionRole: connectingSceneSession.role)
+        }
+        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+    }
+}
